af914aab6befcbf903dfcc653e36e54144fe0301
Denise <deniselewis1120@gmail.com>
2024-05-20 13:58:43 -0700

    Install NativeWind

--- app/index.jsx
+++ app/index.jsx
@@ -4,8 +4,8 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className='flex-1 justify-center items-center bg-white'>
+      <Text className='text-3xl'>Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
